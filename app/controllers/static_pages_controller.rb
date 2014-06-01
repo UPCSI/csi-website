90d1d7dfde64f91a21287570d1e489ed72f83e65
nmcalabroso <nmcalabroso@up.edu.ph>
2014-06-02 06:35:54 +0800

    Configures routes for the static pages.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,14 @@
 class StaticPagesController < ApplicationController
 
   def home
+    render 'static_pages/home/index'
   end
 
   def about
+    render 'static_pages/about/index'
   end
 
   def contact_us
+    render 'static_pages/about/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-
-  #get 'projects/index'
-  #get 'home/index'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  get '/admin' => 'admin#index'
-
-  root 'static_pages#homes'
-  end
+  root 'static_pages#home'
+  
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact_us'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
